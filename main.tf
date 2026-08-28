b1ecd3e1c4fc690c9b5ce43bc90680656f180c63
Minsik Lee <minsikl@gmail.com>
2026-08-28 04:06:10 -0400

    add datasource for vsi image

--- main.tf
+++ main.tf
@@ -1,34 +1,5 @@
-data "ibm_resource_group" "rg" {
-  name = var.resource_group_name
-}
-
-resource "ibm_is_vpc" "vpc" {
-  name           = "${var.student_id}-vpc"
-  resource_group = data.ibm_resource_group.rg.id
-}
-
-resource "ibm_is_subnet" "subnet" {
-  name                     = "${var.student_id}-subnet"
-  vpc                      = ibm_is_vpc.vpc.id
-  zone                     = "${var.region}-1"
-  resource_group           = data.ibm_resource_group.rg.id
-  total_ipv4_address_count = 256
-}
-
-resource "ibm_is_security_group" "sg" {
-  name           = "${var.student_id}-sg"
-  vpc            = ibm_is_vpc.vpc.id
-  resource_group = data.ibm_resource_group.rg.id
-}
-
-resource "ibm_is_security_group_rule" "ssh_in" {
-  group     = ibm_is_security_group.sg.id
-  direction = "inbound"
-  remote    = var.allowed_ssh_cidr
-  tcp {
-    port_min = 22
-    port_max = 22
-  }
+data "ibm_is_image" "vsi_image" {
+  name = var.vsi_image_name
 }
 
 resource "ibm_is_instance" "vsi" {
@@ -36,7 +7,7 @@ resource "ibm_is_instance" "vsi" {
   vpc            = ibm_is_vpc.vpc.id
   zone           = "${var.region}-1"
   profile        = var.vsi_profile
-  image          = var.vsi_image_name
+  image          = data.ibm_is_image.vsi_image.id   # 이름이 아니라 조회된 ID 사용
   keys           = [var.ssh_key_name]
   resource_group = data.ibm_resource_group.rg.id
 
